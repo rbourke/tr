cb2bda20523ec048488bfd81ba894d710e94b98f
Richard Allan Bourke <rbourke@gmail.com>
2018-05-15 02:13:10 +0800

    make indentation consistent

--- src/test/robot/Robot.spec.js
+++ src/test/robot/Robot.spec.js
@@ -3,130 +3,130 @@ let table = new (require('../../lib/table/Table.js')).Table(config.table.dimensi
 let robot = new (require('../../lib/robot/Robot.js')).Robot(table, config.robot);
 
 it('ignores LEFT commands before the robot has been placed', () => {
-  expect(robot.processInput("LEFT")).toBe(false);
+    expect(robot.processInput("LEFT")).toBe(false);
 });
 
 it('ignores RIGHT commands before the robot has been placed', () => {
-  expect(robot.processInput("RIGHT")).toBe(false);
+    expect(robot.processInput("RIGHT")).toBe(false);
 });
 
 it('ignores MOVE commands before the robot has been placed', () => {
-  expect(robot.processInput("MOVE")).toBe(false);
+    expect(robot.processInput("MOVE")).toBe(false);
 });
 
 it('ignores REPORT commands before the robot has been placed', () => {
-  expect(robot.processInput("REPORT")).toBe(false);
+    expect(robot.processInput("REPORT")).toBe(false);
 });
 
 it('can be placed at a valid position', () => {
-  var result = robot.placeRobotAtPosition(0, 0, "NORTH");
-  expect(result).toBeTruthy();
+    let result = robot.placeRobotAtPosition(0, 0, "NORTH");
+    expect(result).toBeTruthy();
 });
 
 it('cannot be placed at an invalid position', () => {
-  var result = robot.placeRobotAtPosition(-1, -1, "NORTH");
-  expect(result).toBeFalsy();
+    let result = robot.placeRobotAtPosition(-1, -1, "NORTH");
+    expect(result).toBeFalsy();
 });
 
 it('can turn left when facing NORTH', () => {
-  robot.placeRobotAtPosition(0, 0, "NORTH");
-  robot.turnLeft();
-  expect(robot.getDirectionFacing()).toBe("WEST");
+    robot.placeRobotAtPosition(0, 0, "NORTH");
+    robot.turnLeft();
+    expect(robot.getDirectionFacing()).toBe("WEST");
 });
 
 it('can turn left when facing EAST', () => {
-  robot.placeRobotAtPosition(0, 0, "EAST");
-  robot.turnLeft();
-  expect(robot.getDirectionFacing()).toBe("NORTH");
+    robot.placeRobotAtPosition(0, 0, "EAST");
+    robot.turnLeft();
+    expect(robot.getDirectionFacing()).toBe("NORTH");
 });
 
 it('can turn left when facing SOUTH', () => {
-  robot.placeRobotAtPosition(0, 0, "SOUTH");
-  robot.turnLeft();
-  expect(robot.getDirectionFacing()).toBe("EAST");
+    robot.placeRobotAtPosition(0, 0, "SOUTH");
+    robot.turnLeft();
+    expect(robot.getDirectionFacing()).toBe("EAST");
 });
 
 it('can turn left when facing WEST', () => {
-  robot.placeRobotAtPosition(0, 0, "WEST");
-  robot.turnLeft();
-  expect(robot.getDirectionFacing()).toBe("SOUTH");
+    robot.placeRobotAtPosition(0, 0, "WEST");
+    robot.turnLeft();
+    expect(robot.getDirectionFacing()).toBe("SOUTH");
 });
 
 it('can turn right when facing NORTH', () => {
-  robot.placeRobotAtPosition(0, 0, "NORTH");
-  robot.turnRight();
-  expect(robot.getDirectionFacing()).toBe("EAST");
+    robot.placeRobotAtPosition(0, 0, "NORTH");
+    robot.turnRight();
+    expect(robot.getDirectionFacing()).toBe("EAST");
 });
 
 it('can turn right when facing EAST', () => {
-  robot.placeRobotAtPosition(0, 0, "EAST");
-  robot.turnRight();
-  expect(robot.getDirectionFacing()).toBe("SOUTH");
+    robot.placeRobotAtPosition(0, 0, "EAST");
+    robot.turnRight();
+    expect(robot.getDirectionFacing()).toBe("SOUTH");
 });
 
 it('can turn right when facing SOUTH', () => {
-  robot.placeRobotAtPosition(0, 0, "SOUTH");
-  robot.turnRight();
-  expect(robot.getDirectionFacing()).toBe("WEST");
+    robot.placeRobotAtPosition(0, 0, "SOUTH");
+    robot.turnRight();
+    expect(robot.getDirectionFacing()).toBe("WEST");
 });
 
 it('can turn right when facing WEST', () => {
-  robot.placeRobotAtPosition(0, 0, "WEST");
-  robot.turnRight();
-  expect(robot.getDirectionFacing()).toBe("NORTH");
+    robot.placeRobotAtPosition(0, 0, "WEST");
+    robot.turnRight();
+    expect(robot.getDirectionFacing()).toBe("NORTH");
 });
 
 it('can move EAST from position: 0,0', () => {
-  robot.placeRobotAtPosition(0, 0, "EAST");
-  robot.moveForward();
-  expect(robot.getXordinate()).toBe(1);
+    robot.placeRobotAtPosition(0, 0, "EAST");
+    robot.moveForward();
+    expect(robot.getXordinate()).toBe(1);
 });
 
 it('can move NORTH from position: 0,0', () => {
-  robot.placeRobotAtPosition(0, 0, "NORTH");
-  robot.moveForward();
-  expect(robot.getYordinate()).toBe(1);
+    robot.placeRobotAtPosition(0, 0, "NORTH");
+    robot.moveForward();
+    expect(robot.getYordinate()).toBe(1);
 });
 
 it('cannot move WEST from position: 0,0', () => {
-  robot.placeRobotAtPosition(0, 0, "WEST");
-  var result = robot.moveForward();
-  expect(result).toBeFalsy();
+    robot.placeRobotAtPosition(0, 0, "WEST");
+    let result = robot.moveForward();
+    expect(result).toBeFalsy();
 });
 
 it('cannot move SOUTH from position: 0,0', () => {
-  robot.placeRobotAtPosition(0, 0, "SOUTH");
-  var result = robot.moveForward();
-  expect(result).toBeFalsy();
+    robot.placeRobotAtPosition(0, 0, "SOUTH");
+    let result = robot.moveForward();
+    expect(result).toBeFalsy();
 });
 
 it('cannot move EAST from position: max,max', () => {
-  robot.placeRobotAtPosition(table.getWidth() - 1, table.getLength() - 1, "EAST");
-  var result = robot.moveForward();
-  expect(result).toBeFalsy();
+    robot.placeRobotAtPosition(table.getWidth() - 1, table.getLength() - 1, "EAST");
+    let result = robot.moveForward();
+    expect(result).toBeFalsy();
 });
 
 it('cannot move NORTH from position: max,max', () => {
-  robot.placeRobotAtPosition(table.getWidth() - 1, table.getLength() - 1, "NORTH");
-  var result = robot.moveForward();
-  expect(result).toBeFalsy();
+    robot.placeRobotAtPosition(table.getWidth() - 1, table.getLength() - 1, "NORTH");
+    let result = robot.moveForward();
+    expect(result).toBeFalsy();
 });
 
 it('can move WEST from position: max,max', () => {
-  robot.placeRobotAtPosition(table.getWidth() - 1, table.getLength() - 1, "WEST");
-  robot.moveForward();
-  expect(robot.getXordinate()).toBe(table.getWidth() - 2);
+    robot.placeRobotAtPosition(table.getWidth() - 1, table.getLength() - 1, "WEST");
+    robot.moveForward();
+    expect(robot.getXordinate()).toBe(table.getWidth() - 2);
 });
 
 it('cannot move SOUTH from position: max,max', () => {
-  robot.placeRobotAtPosition(table.getWidth() - 1, table.getLength() - 1, "SOUTH");
-  robot.moveForward();
-  expect(robot.getYordinate()).toBe(table.getLength() - 2);
+    robot.placeRobotAtPosition(table.getWidth() - 1, table.getLength() - 1, "SOUTH");
+    robot.moveForward();
+    expect(robot.getYordinate()).toBe(table.getLength() - 2);
 });
 
 it('ignores invalid commands', () => {
-  let result = robot.processInput("FLY");
-  expect(result).toBeFalsy();
+    let result = robot.processInput("FLY");
+    expect(result).toBeFalsy();
 });
 
 it('can parse & execute a valid PLACE command', () => {
@@ -152,29 +152,29 @@ it('ignores PLACE commands with an invalid direction', () => {
 });
 
 it('can parse & execute a MOVE command', () => {
-  robot.processInput("PLACE 1,2,EAST");
-  robot.processInput("MOVE");
-  expect(robot.getXordinate()).toBe(2);
+    robot.processInput("PLACE 1,2,EAST");
+    robot.processInput("MOVE");
+    expect(robot.getXordinate()).toBe(2);
 });
 
 it('can parse & execute a LEFT command', () => {
-  robot.processInput("PLACE 1,2,EAST");
-  robot.processInput("LEFT");
-  expect(robot.getDirectionFacing()).toBe("NORTH");
+    robot.processInput("PLACE 1,2,EAST");
+    robot.processInput("LEFT");
+    expect(robot.getDirectionFacing()).toBe("NORTH");
 });
 
 it('can parse & execute a RIGHT command', () => {
-  robot.processInput("PLACE 1,2,EAST");
-  robot.processInput("RIGHT");
-  expect(robot.getDirectionFacing()).toBe("SOUTH");
+    robot.processInput("PLACE 1,2,EAST");
+    robot.processInput("RIGHT");
+    expect(robot.getDirectionFacing()).toBe("SOUTH");
 });
 
 it('can parse & execute a REPORT command', () => {
-  robot.processInput("PLACE 1,2,EAST");
-  expect(robot.processInput("REPORT")).toBe(true);
+    robot.processInput("PLACE 1,2,EAST");
+    expect(robot.processInput("REPORT")).toBe(true);
 });
 
 it('always creates a new robot', () => {
-  let robot = (require('../../lib/robot/Robot.js')).Robot(table, config.robot);
-  expect(robot).toBeInstanceOf((require('../../lib/robot/Robot.js')).Robot);
+    let robot = (require('../../lib/robot/Robot.js')).Robot(table, config.robot);
+    expect(robot).toBeInstanceOf((require('../../lib/robot/Robot.js')).Robot);
 });
--- src/test/table/Table.spec.js
+++ src/test/table/Table.spec.js
@@ -2,18 +2,18 @@ const config = require('../../config/properties.json');
 let table = new (require('../../lib/table/Table.js')).Table(config.table.dimensions);
 
 it('accepts valid dimensions', () => {
-  table.setDimensions(5, 5);
-  expect(table.getLength()).toBe(5);
-  expect(table.getWidth()).toBe(5);
+    table.setDimensions(5, 5);
+    expect(table.getLength()).toBe(5);
+    expect(table.getWidth()).toBe(5);
 });
 
 it('ignores invalid dimensions', () => {
-  table.setDimensions(-1, -1);
-  expect(table.getLength()).not.toBe(-1);
-  expect(table.getWidth()).not.toBe(-1);
+    table.setDimensions(-1, -1);
+    expect(table.getLength()).not.toBe(-1);
+    expect(table.getWidth()).not.toBe(-1);
 });
 
 it('always creates a new table', () => {
-  let table = (require('../../lib/table/Table.js')).Table(config.table.dimensions);
-  expect(table).toBeInstanceOf((require('../../lib/table/Table.js')).Table);
+    let table = (require('../../lib/table/Table.js')).Table(config.table.dimensions);
+    expect(table).toBeInstanceOf((require('../../lib/table/Table.js')).Table);
 });
